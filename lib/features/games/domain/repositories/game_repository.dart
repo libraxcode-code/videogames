b7484a5ed26a414ded955ee2c52b4ce98784fdd8
librax <libraxcode@gmail.com>
2026-09-18 21:17:28 +0700

    feat(games): implement domain models, usecases, and remote data source

---
+++ lib/features/games/domain/repositories/game_repository.dart
@@ -0,0 +1,8 @@
+﻿import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../entities/game_entity.dart';
+
+abstract class GameRepository {
+  Future<Either<Failure, List<GameEntity>>> getGames({bool forceRefresh = false});
+  Future<Either<Failure, GameEntity>> getGameById(String id);
+}
---
+++ lib/features/games/domain/usecases/get_games_usecase.dart
@@ -0,0 +1,26 @@
+﻿import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecase/usecase.dart';
+import '../entities/game_entity.dart';
+import '../repositories/game_repository.dart';
+
+class GetGamesParams extends Equatable {
+  final bool forceRefresh;
+
+  const GetGamesParams({this.forceRefresh = false});
+
+  @override
+  List<Object?> get props => [forceRefresh];
+}
+
+class GetGamesUseCase implements UseCase<List<GameEntity>, GetGamesParams> {
+  final GameRepository repository;
+
+  const GetGamesUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, List<GameEntity>>> call(GetGamesParams params) async {
+    return await repository.getGames(forceRefresh: params.forceRefresh);
+  }
+}
